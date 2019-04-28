6aeef833408b7f52f39f5704766a56aee253f02d
norisio <k.exa.apostrophe@gmail.com>
2019-04-28 19:11:18 +0900

    implement addition and subtraction

--- rvcc.cpp
+++ rvcc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char** argv){
   if(argc != 2){
@@ -6,11 +7,33 @@ int main(int argc, char** argv){
     return 1;
   }
 
+
+  char* p = argv[1];
+
   std::cout <<
     ".global main\n"
     "\n"
     "main:\n"
-    "  li   a0, " << argv[1] << "\n"
+    "  li   a0, " << std::strtol(p, &p, 10) << "\n";
+
+  while(*p){
+    if(*p == '+'){
+      p++;
+      std::cout << "  addi  a0, a0, " << std::strtol(p, &p, 10) << "\n";
+      continue;
+    }
+    if(*p == '-'){
+      p++;
+      std::cout << "  addi  a0, a0, -" << std::strtol(p, &p, 10) << "\n";
+      continue;
+    }
+
+    std::cerr << "予期しない文字です: " << (int)*p << std::endl;
+    return 1;
+  }
+
+
+  std::cout <<
     "  ret\n" << std::endl;
 
   return 0;
