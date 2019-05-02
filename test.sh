af1a9ca53e473da6a45126225925bc2dfe3e923e
norisio <k.exa.apostrophe@gmail.com>
2019-05-02 18:03:02 +0900

    test relational, equality

--- test.sh
+++ test.sh
@@ -19,15 +19,23 @@ try() {
   fi
 }
 
-#single integer
-try 0 0
-try 42 42
+# relational, equality
+try 1 '5==5'
+try 0 '5!=5'
+try 1 '5 == 3+2'
+try 1 '3<5'
+try 0 '5<5'
+try 1 '3<5'
+try 1 '3<=5'
+try 1 '1+1 >= 2'
+try 0 '1+1 >  2'
 
-#addition, subtraction
-try 21 '5+20-4'
 
-#tokenize
-try 21 '5    + 20            - 4'
+# unary minus and plus
+try 10 '9+-(-3)-(+2)'
+
+#parentheses
+try 9 '(1+2)*3'
 
 #multiplication, division
 try 6 '3*2'
@@ -35,11 +43,15 @@ try 6 '18/3'
 try 7 '1+3*2'
 try 20 '1+10/2*4-1'
 
-#parentheses
-try 9 '(1+2)*3'
+#tokenize
+try 21 '5    + 20            - 4'
 
-# unary minus and plus
-try 10 '9+-(-3)-(+2)'
+#addition, subtraction
+try 21 '5+20-4'
+
+#single integer
+try 0 0
+try 42 42
 
 echo OK
 
